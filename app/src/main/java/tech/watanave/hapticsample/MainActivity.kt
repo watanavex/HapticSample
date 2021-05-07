e7a8285280cd994cfb474685dbd5ccfa6abb9ec8
watanavex <y_watanabe@yumemi.co.jp>
2021-05-07 15:59:45 +0900

    Implement VibrationEffect

--- app/src/main/java/tech/watanave/hapticsample/MainActivity.kt
+++ app/src/main/java/tech/watanave/hapticsample/MainActivity.kt
@@ -1,9 +1,14 @@
 package tech.watanave.hapticsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
+import android.os.VibrationEffect.EFFECT_CLICK
+import android.os.VibrationEffect.EFFECT_DOUBLE_CLICK
+import android.os.Vibrator
 import android.view.HapticFeedbackConstants.CONFIRM
 import android.view.HapticFeedbackConstants.REJECT
+import androidx.appcompat.app.AppCompatActivity
 import tech.watanave.hapticsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +24,23 @@ class MainActivity : AppCompatActivity() {
 
         binding.rejectButton.setOnClickListener {
             binding.rejectButton.performHapticFeedback(REJECT)
+        }
+
+        val vibrator = getSystemService(Vibrator::class.java)
+
+        binding.vibrationClickButton.setOnClickListener {
+            val effect = VibrationEffect.createPredefined(EFFECT_CLICK)
+            vibrator.vibrate(effect)
+        }
+
+        binding.vibrationDoubleClickButton.setOnClickListener {
+            val effect = VibrationEffect.createPredefined(EFFECT_DOUBLE_CLICK)
+            vibrator.vibrate(effect)
+        }
+
+        binding.vibrationCustom.setOnClickListener {
+            val effect = VibrationEffect.createWaveform(longArrayOf(3, 150, 3), intArrayOf(DEFAULT_AMPLITUDE, 0, DEFAULT_AMPLITUDE), -1)
+            vibrator.vibrate(effect)
         }
     }
 }
